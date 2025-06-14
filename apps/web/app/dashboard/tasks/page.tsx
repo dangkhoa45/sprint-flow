bcdd8f9bdc2cb4eaf344fb643e26c63ad043ddc1
Khoai <khoa0310st@gmail.com>
2025-06-14 20:52:33 +0700

    delete file

--- apps/web/app/dashboard/tasks/page.tsx
+++
@@ -1,5 +0,0 @@
-import TaskManagement from "./components/TaskManagement";
-
-export default function TasksPage() {
-  return <TaskManagement />;
-}
--- apps/web/app/dashboard/theme-test/page.tsx
+++
@@ -1,5 +0,0 @@
-import ColorShowcase from "../../../components/ColorShowcase";
-
-export default function ThemeTestPage() {
-  return <ColorShowcase />;
-}
